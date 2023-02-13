f9dee212b836c51e8aaf1758358a9ef02a95c888
nagatomorika <rika.nagatomo@divx.co.jp>
2023-02-13 13:48:33 +0900

    ボタンを押すとコンソールに情報が表示される

--- front/app/src/app/page.js
+++ front/app/src/app/page.js
@@ -1,10 +1,22 @@
+'use client'
 import Layout from "./layout";
 import Styles from "./page.module.css";
+import axios from "axios";
+
+function handleClick() {
+  console.log("increment like count")
+ }
 
 export default function Home() {
-  return <Layout>
-    <div className={Styles.button}>
-    <button>Get Response</button>
-    </div>
-  </Layout>;
+  return(
+    <Layout>
+      <div  className={Styles.button}>
+        <button onClick={handleClick}>
+          Get Response
+        </button>
+      </div>
+    </Layout>
+  );
 }
+
+
